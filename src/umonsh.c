043616f1a92cc7c65c99a5ea61eccd5cfa623038
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2025-03-16 19:02:48 +0100

    gestion des arguments

--- src/umonsh.c
+++ src/umonsh.c
@@ -1,14 +1,25 @@
 /***********************************************
  * Project 2: Shell
  * 
- * Group Number : 
- * Students     : 
+ * Group Number : 10
+ * Students     : Bryan Pax, Valentin Libouton
  * 
  * Please add details if compilation of your project is not
  * straightforward (for instance, if you use multiple files).
  ***********************************************/
+# include <unistd.h>
+# include <string.h>
+
+const char *error_message = "An error has occured\n";
 
 int main(int argc, char *argv[]) {
-    //TODO
+    if (argc == 1) {
+        // Mode interactif
+    } else if (argc == 2) {
+        // Mode batch
+    } else {
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
     return 0;
 }
