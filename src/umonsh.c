4c8dde29751a28594be4f6a0a803ad7b54ee393a
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2025-03-16 16:41:19 +0100

    ajout du code de base

---
+++ src/umonsh.c
@@ -0,0 +1,14 @@
+/***********************************************
+ * Project 2: Shell
+ * 
+ * Group Number : 
+ * Students     : 
+ * 
+ * Please add details if compilation of your project is not
+ * straightforward (for instance, if you use multiple files).
+ ***********************************************/
+
+int main(int argc, char *argv[]) {
+    //TODO
+    return 0;
+}
