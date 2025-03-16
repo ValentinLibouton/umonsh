82cb4fd4396c99f6aa5b9359f92265e9c5a8d137
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2025-03-16 21:49:41 +0100

    début implémentation boucle interactive

--- src/umonsh.c
+++ src/umonsh.c
@@ -7,16 +7,31 @@
  * Please add details if compilation of your project is not
  * straightforward (for instance, if you use multiple files).
  ***********************************************/
-# include <unistd.h>
-# include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 const char *error_message = "An error has occured\n";
+
+void interactive_mode(FILE *input) {
+    // Mode interactif : umonsh>
+    char *buffer = NULL;
+    size_t size = 0;
+    // Ce qui suit devra être dans une boucle infinie ...
+    printf("umonsh> ");
+    if (getline(&buffer, &size, stdin) == -1) {
+        //break; // EOF ou erreur ...
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         // Mode interactif
+        interactive_mode(stdin);
     } else if (argc == 2) {
         // Mode batch
+        // A développer plus tard... (pas maintenant)
     } else {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
