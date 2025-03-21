1bda2e7afb18eab6ac8915f03034c6f6c76632bc
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2025-03-21 09:40:51 +0100

    intégration de la boucle interactive, parsing des arguments, fonction exit fonctionne

--- src/umonsh.c
+++ src/umonsh.c
@@ -12,13 +12,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VERBOSE 1
+
 const char *error_message = "An error has occured\n";
 
 int parse_command(char *command, char *args[], int max_args) {
     int argc = 0;
-    char *token = strtok(command, " ");
-    //TODO pour valentin
-
+    char *token = strtok(command, " "); //Commencer à découper la chaîne 'command'
+    
+    while (token != NULL && argc < max_args -1) {
+        args[argc++] = token;
+        token = strtok(NULL, " "); // Continuer à découper le reste de 'command'
+    }
+    args[argc] = NULL;
     return argc;
 }
 
@@ -26,25 +32,37 @@ void interactive_mode(FILE *input) {
     // Mode interactif : umonsh>
     char *command = NULL;
     size_t command_size = 0;
+    while(1) {
+        printf("umonsh> ");
+        if (getline(&command, &command_size, stdin) == -1) {
+            free(command);
+            exit(0);
+        }
 
-    // Ce qui suit devra être dans une boucle infinie ...
-    printf("umonsh> ");
-    if (getline(&command, &command_size, stdin) == -1) {
-        //break; // EOF ou erreur ...
-        exit(0);
-    }
-
-    command[strcspn(command, "\n")] = '\0'; // supression du \n
-    printf("%s - %ld octets", command, command_size);
-
-    if (strcmp(command, "exit") == 0) { // quitter le shell
-        printf("Sortie du shell Umonsh");
-        exit(0);
-    }
-
-
-
+        command[strcspn(command, "\n")] = '\0'; // supression du \n
     
+        if (VERBOSE) {
+            printf("[Verbose mode=1] Command: %s - Allocated %ld octets, Used %ld chars\n", command, command_size, strlen(command));
+        }
+
+        char *args[100];
+        int argc = parse_command(command, args, 100);
+
+        if (argc == 0) {
+            continue;  // Ligne vide, on repart dans la boucle
+        }
+
+        if (strcmp(args[0], "exit") == 0) {
+            free(command);
+            printf("Sortie du shell Umonsh\n");
+            exit(0);
+        }
+
+        //printf("Commande : %s\n", args[0]);
+        for (int i = 1; i < argc; i++) {
+            printf("Arg %d : %s\n", i, args[i]);
+        }    
+    }
 }
 
 int main(int argc, char *argv[]) {
